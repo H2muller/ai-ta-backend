40eb0ce7cfad5872c2e74654eba1c9036cd0e375
Kastan Day <kastanvday@gmail.com>
2025-04-02 09:30:04 -0700

    Refine Qdrant migration script, very efficient and complete now

--- ai_ta_backend/utils/migrate_cropwizard.py
+++ ai_ta_backend/utils/migrate_cropwizard.py
@@ -1,44 +1,45 @@
-# OLD DB: http://ec2-3-81-233-108.compute-1.amazonaws.com:6333/
-# New DB: cropwizard:6333
 import os
 
 from qdrant_client import QdrantClient, models
 
-prod_client = QdrantClient(url=os.environ['QDRANT_URL'], port=6333, https=False, api_key=os.environ['QDRANT_API_KEY'])
+source_vector_db = QdrantClient(url=os.environ['QDRANT_URL'],
+                                port=6333,
+                                https=False,
+                                api_key=os.environ['QDRANT_API_KEY'])
 
-cropwizard_client = QdrantClient(url=os.environ['CROPWIZARD_QDRANT_URL'],
-                                 port=6333,
-                                 https=False,
-                                 api_key=os.environ['QDRANT_API_KEY'])
+destination_vector_db = QdrantClient("http://localhost", port=6333, https=False, api_key=os.environ['QDRANT_API_KEY'])
 
-cropwizard_collection_name = "cropwizard"
+source_collection_name = os.environ['QDRANT_COLLECTION_NAME']
+destination_collection_name = os.environ['QDRANT_COLLECTION_NAME']
 vector_size = 1536
 
-cropwizard_client.recreate_collection(
-    collection_name=cropwizard_collection_name,
+destination_vector_db.recreate_collection(
+    collection_name=destination_collection_name,
     on_disk_payload=True,
-    optimizers_config=models.OptimizersConfigDiff(indexing_threshold=1_000_000),
+    optimizers_config=models.OptimizersConfigDiff(indexing_threshold=10_000_000),
     vectors_config=models.VectorParams(
         size=vector_size,
         distance=models.Distance.COSINE,
         on_disk=True,
-        hnsw_config=models.HnswConfigDiff(on_disk=True),
+        hnsw_config=models.HnswConfigDiff(on_disk=False),  # In memory HNSW.
     ),
 )
 
 offset = None
 counter = 0
+batch_size = 250
+
 while True:
-  res = prod_client.scroll(
-      collection_name=os.environ['QDRANT_COLLECTION_NAME'],
-      scroll_filter=models.Filter(must=[
-          models.FieldCondition(key="course_name", match=models.MatchValue(value="cropwizard-1.5")),
-      ]),
-      limit=100,
+  res = source_vector_db.scroll(
+      collection_name=source_collection_name,
+      #   scroll_filter=models.Filter(must=[
+      #       models.FieldCondition(key="course_name", match=models.MatchValue(value="cropwizard-1.5")),
+      #   ]),
+      limit=batch_size,
       with_payload=True,
       with_vectors=True,
       offset=offset)
-  counter += 100
+  counter += batch_size
   print(f"Processing records: {counter}")
   # print(res[0])  # Print the records
 
@@ -48,7 +49,7 @@ while True:
       vector=point.vector,
   ) for point in res[0]]
 
-  cropwizard_client.upsert(collection_name=cropwizard_collection_name, points=points)
+  destination_vector_db.upsert(wait=False, collection_name=destination_collection_name, points=points)
 
   offset = res[1]  # Get next_page_offset
   if offset is None:  # If next_page_offset is None, we've reached the last page
